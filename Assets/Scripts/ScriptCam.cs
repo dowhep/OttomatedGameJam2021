3e0e2bed6cbdbe4db3707de926a354507855c9c8
dowhep <caogangshen@gmail.com>
2021-10-20 12:42:40 -0700

    init

---
+++ Assets/Scripts/ScriptCam.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ScriptCam : MonoBehaviour
+{
+ 
+    #region Public Fields
+    #endregion
+ 
+    #region Unity Methods
+ 
+    void Start()
+    {
+    }
+ 
+    void Update()
+    {
+    }
+ 
+    #endregion
+ 
+    #region Private Methods
+    #endregion
+}
